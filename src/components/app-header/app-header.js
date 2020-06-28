0b93b146e3449193633b3657abec241436c2ed81
Alex <nishtidight@ukr.net>
2020-06-28 12:42:34 +0300

    menu loaded

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import cartIcon from './shopping-cart-solid.svg';
+import {Link} from 'react-router-dom';
 import './app-header.scss';
 
 const AppHeader = ({total}) => {
     return (
         <header className="header">
-            <a className="header__link" href="#">
+            <Link className="header__link" to="/menu/">
                 Menu
-            </a>
-            <a className="header__link" href="#">
+            </Link>
+            <Link className="header__link" to="/total/">
                 <img className="header__cart" src={cartIcon} alt="cart"></img>
                 Total: {total} $
-            </a>
+            </Link>
         </header>
     )
 };
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {MainPage, CartPage} from '../pages';
 import AppHeader from '../app-header';
+import {Route, Switch} from 'react-router-dom';
 
 import Background from './food-bg.jpg';
 
@@ -8,8 +9,11 @@ const App = () => {
     return (
         <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
             <AppHeader total={50}/>
-            <MainPage/>
-            <CartPage/>
+            <Switch>
+                <Route path='/' exact component={MainPage}/>
+                <Route path='/total' exact component={CartPage}/>
+                <Route exact component={MainPage}/>
+            </Switch>
         </div>
     )
 }
